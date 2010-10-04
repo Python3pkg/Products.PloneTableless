2780e7fb1b08068b14d6ce171cc8f32a980779ff
Raptus <dev@raptus.com>
2010-10-04 13:14:01 +0000

    add a missing wrapper make main-template xml-valid

--- Products/PloneTableless/interfaces.py
+++ Products/PloneTableless/interfaces.py
@@ -1,7 +1,6 @@
 from plonetheme.classic.browser.interfaces import ILegacyThemeSpecific
 
 class IThemeSpecific(ILegacyThemeSpecific):
-    """ Plone tableless depends on the classic theme. Use this layer
-        to make sure it continues to get the old viewlet order.
+    """ Plone tableless depends on the classic theme.
     """
     
