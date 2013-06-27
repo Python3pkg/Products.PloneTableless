27bc89354110629ff854693edbe552317b271448
Simon Käser <skaeser@raptus.com>
2013-06-27 15:42:10 +0200

    Switched DOCTYPE to HTML5 and added sl and sr CSS classes to #visual-portal-wrapper

--- Products/PloneTableless/skins/plone_tableless/hide_columns.js
+++ Products/PloneTableless/skins/plone_tableless/hide_columns.js
@@ -1,4 +1,5 @@
 if(document.getElementById) {
+  var wrapper = document.getElementById('visual-portal-wrapper');
   var content = document.getElementById('portal-column-content');
   if(content) {
     String.prototype.trim = function() {
@@ -8,6 +9,7 @@ if(document.getElementById) {
     var sr = document.getElementById('portal-column-two');
     sl = sl && sl.innerHTML.trim();
     sr = sr && sr.innerHTML.trim();
+    wrapper.className = (sl && sr && 'sl sr') || (sl && 'sl') || (sr && 'sr') || '';
     content.className = (sl && sr && 'sl sr') || (sl && 'sl') || (sr && 'sr') || '';
   }
 }
